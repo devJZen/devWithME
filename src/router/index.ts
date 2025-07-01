6adbe9775047eb01a86efbff11ee64aa63fd9991
juseon <quasar.joo@gmail.com>
2025-07-01 19:11:32 +0900

    init Category

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import QuestionDetailView from '../views/QuestionDetailView.vue'
 import Aboutview from '@/views/Aboutview.vue'
+import Categoryview from '@/views/Categoryview.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,6 +17,12 @@ const router = createRouter({
       path: '/about',
       name: 'about',
       component: Aboutview,
+    },
+    {
+      path: '/category/', // 카테고리별 질문 목록
+      name: 'category-questions',
+      props: true, // 라우트 매개변수를 컴포넌트 props로 전달
+      component: Categoryview,
     },
     {
       path: '/question/:id', // 질문 ID에 따라 상세 페이지
