5c2d0d031e9458dc8a5ec1a2f10bb6eb54b9663e
devJZen <quasar.joo@google.com>
2025-07-02 01:02:03 +0900

    init Test

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,7 @@ import HomeView from '../views/HomeView.vue'
 import QuestionDetailView from '../views/QuestionDetailView.vue'
 import Aboutview from '@/views/Aboutview.vue'
 import Categoryview from '@/views/Categoryview.vue'
+import Testview from '@/views/Testview.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -19,10 +20,14 @@ const router = createRouter({
       component: Aboutview,
     },
     {
-      path: '/category/', // 카테고리별 질문 목록
+      path: '/category', // 카테고리별 질문 목록
       name: 'category-questions',
-      props: true, // 라우트 매개변수를 컴포넌트 props로 전달
       component: Categoryview,
+    },
+    {
+      path: '/test', // 커스텀 테스트 페이지
+      name: 'test',
+      component: Testview,
     },
     {
       path: '/question/:id', // 질문 ID에 따라 상세 페이지
